f2d11543301285cd4393ecb26f1aa2cb8a457e27
Hau Le <leconghau.me@gmail.com>
2020-04-26 11:26:21 +0700

    copy event

--- script.js
+++ script.js
@@ -118,3 +118,32 @@ function updateOutput() {
 }
 window.addEventListener('resize', updateOutput);
 updateOutput();
+
+function getSelectionHtml() {
+  var html = '';
+  if (typeof window.getSelection != 'undefined') {
+    var sel = window.getSelection();
+    if (sel.rangeCount) {
+      var container = document.createElement('div');
+      for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+        container.appendChild(sel.getRangeAt(i).cloneContents());
+      }
+      html = container.innerHTML;
+    }
+  } else if (typeof document.selection != 'undefined') {
+    if (document.selection.type == 'Text') {
+      html = document.selection.createRange().htmlText;
+    }
+  }
+  return html;
+}
+
+document.addEventListener('copy', function (e) {
+  alert(getSelectionHtml());
+  var text = window
+    .getSelection()
+    .toString()
+    .replace(/[\n\r]+/g, '');
+  e.clipboardData.setData('text/plain', text);
+  e.preventDefault();
+});
